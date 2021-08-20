c2853f821726148c1330f09935fd0598f61d7c70
Michael Tew <mike.tew@gmail.com>
2021-08-20 08:44:00 -0700

    WIP: Implementing a sides dropdown menu.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from random import randint
 
 
 class AppLayout(BoxLayout):
-    pass
+
+    def roll_dice(self):
+        print("Roll Dice")
+        self.ids.dice.text = str(randint(1, 6))
+
+    def select_sides(self, value):
+        print(value)
+        self.ids.dice.text = value
 
 
 class DiceRollingApp(App):
